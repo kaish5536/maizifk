f9d547bd5652e0e995a9c82c3fcb484c68f6be26
kaish5536 <lighstar.wang@live.com>
2016-09-07 23:11:54 +0800

    extends

--- app.py
+++ app.py
@@ -7,9 +7,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template('index.html',
-							title="<h1>Helle world</h1>",
-							body='## Header2')
+	return render_template('index.html')
 
 @app.template_filter('md')
 def markdown_to_html(txt):
